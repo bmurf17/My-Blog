31d3ad40e192e64633b0f2003ef292d3461e845b
Brendan Murphy <brendan.murphy@hope.edu>
2022-04-06 09:41:40 -0400

    Set up create post section

--- src/Components/Create Post/CreatePost.tsx
+++ src/Components/Create Post/CreatePost.tsx
@@ -17,26 +17,52 @@ const data = [
 ];
 
 export function CreatePost() {
-  const [value, onChange] = useState("");
+  const [blogContent, setBlogContent] = useState("");
+  const [postName, setPostName] = useState("");
+  const [tags, setTags] = useState<string[]>([]);
 
-  //TODO: Add a form
+  const onSubmit = () => {
+    console.log("PostName: " + postName);
+
+    console.log("tags: " + tags);
+
+    console.log("blog content: " + blogContent);
+  };
+
   return (
-    <div>
+    <form onSubmit={onSubmit}>
       <Text size="xl">Create Your Post</Text>
       <Space h="md" />
-      <TextInput placeholder="My Post" label="Post Name" required />
+      <TextInput
+        placeholder="My Post"
+        label="Post Name"
+        onChange={(e) => {
+          e.preventDefault();
+          setPostName(e.target.value);
+        }}
+        required
+      />
       <Space h="md" />
       <MultiSelect
         data={data}
         label="Pick all the tags you would like"
         placeholder="Pick all that you like"
+        value={tags}
+        onChange={(e) => {
+          var temp: string[] = [""];
+          temp = e;
+          setTags(e);
+        }}
       />
       <Space h="md" />
-      <RichTextEditor value={value} onChange={onChange} />
+      <Text size="xl">Content</Text>
+      <RichTextEditor value={blogContent} onChange={setBlogContent} />
       <Space h="md" />
       <Group position="right">
-        <Button size="md">Submit</Button>
+        <Button onClick={onSubmit} size="md">
+          Submit
+        </Button>
       </Group>
-    </div>
+    </form>
   );
 }
